c2940dba66661c4dbc4828fc4183eab657ba50e4
Greg Horn <greg@kittyhawk.aero>
2020-10-28 22:25:38 -0700

    add C++ hello world visualizer

---
+++ WORKSPACE
@@ -0,0 +1,49 @@
+workspace(name = "basketball")
+
+#load("//:dem_names.bzl", "dem_names", "dem_checksum")
+load("@bazel_tools//tools/build_defs/repo:http.bzl", "http_archive")
+
+http_archive(
+    name = "rules_python",
+    url = "https://github.com/bazelbuild/rules_python/releases/download/0.0.3/rules_python-0.0.3.tar.gz",
+    sha256 = "e46612e9bb0dae8745de6a0643be69e8665a03f63163ac6610c210e80d14c3e4",
+)
+#load("@rules_python//python:repositories.bzl", "py_repositories")
+#py_repositories()
+#
+#load("@rules_python//python:pip.bzl", "pip_repositories")
+#pip_repositories()
+
+
+# boost
+load("@bazel_tools//tools/build_defs/repo:git.bzl", "git_repository")
+
+git_repository(
+    name = "com_github_nelhage_rules_boost",
+    commit = "1e3a69bf2d5cd10c34b74f066054cd335d033d71",
+    remote = "https://github.com/nelhage/rules_boost",
+    shallow_since = "1591047380 -0700",
+)
+
+load("@com_github_nelhage_rules_boost//:boost/boost.bzl", "boost_deps")
+boost_deps()
+
+#[
+#    http_archive(
+#        name = "dem_{}".format(dem_name),
+#        urls = ["https://storage.googleapis.com/state-of-utah-sgid-downloads/lidar/zion-np-2016/QL2/DEMs/{}.zip".format(dem_name)],
+#        sha256 = dem_checksum(dem_name),
+#        build_file_content = """
+#filegroup(
+#    name = "dem_img_{dem_name}",
+#    srcs = [
+#        "{dem_name}.img",
+#        "{dem_name}.img.aux.xml",
+#        "{dem_name}.img.xml",
+#    ],
+#    visibility = ["//visibility:public"],
+#)
+#""".format(dem_name=dem_name)
+#    )
+#    for dem_name in dem_names()
+#]
---
+++ vis.cpp
@@ -0,0 +1,300 @@
+#include <cstdio>
+#include <cstdlib>
+#include <chrono>
+#include <string>
+
+//#define GLEW_STATIC
+#include <GL/glew.h>
+
+#define GLFW_INCLUDE_NONE
+#include <GLFW/glfw3.h>
+
+//#include <GL/gl.h>
+
+#include <SOIL/SOIL.h>
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+// Shader sources
+const GLchar* vertexSource = R"glsl(
+    #version 150 core
+    in vec2 position;
+    in vec3 color;
+    in vec2 texcoord;
+    out vec3 Color;
+    out vec2 Texcoord;
+    uniform mat4 model;
+    uniform mat4 view;
+    uniform mat4 proj;
+    void main()
+    {
+        Color = color;
+        Texcoord = texcoord;
+        gl_Position = proj * view * model * vec4(position, 0.0, 1.0);
+    }
+)glsl";
+const GLchar* fragmentSource = R"glsl(
+    #version 150 core
+    in vec3 Color;
+    in vec2 Texcoord;
+    out vec4 outColor;
+    uniform sampler2D texKitten;
+    uniform sampler2D texPuppy;
+    void main()
+    {
+        outColor = mix(texture(texKitten, Texcoord), texture(texPuppy, Texcoord), 0.5);
+    }
+)glsl";
+
+static void KeyCallback(GLFWwindow* window,
+                        int key,
+                        int scancode __attribute__((unused)),
+                        int action,
+                        int mods __attribute__((unused))) {
+  fprintf(stderr, "Key press! %d\n", key);
+  if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS) {
+    glfwSetWindowShouldClose(window, GLFW_TRUE);
+  }
+}
+
+static void CursorPositionCallback(GLFWwindow* window __attribute__((unused)),
+                                   double xpos,
+                                   double ypos) {
+  fprintf(stderr, "Mouse moved! %.1f %.1f\n", xpos, ypos);
+}
+
+static void MouseButtonCallback(GLFWwindow* window,
+                                int button,
+                                int action,
+                                int mods __attribute__((unused))) {
+  if (button == GLFW_MOUSE_BUTTON_RIGHT && action == GLFW_PRESS) {
+    
+  }
+
+  double xpos, ypos;
+  glfwGetCursorPos(window, &xpos, &ypos);
+  fprintf(stderr, "Mouse button pressed: %d %d (%.1f, %.1f)\n", button, action, xpos, ypos);
+}
+
+static void ScrollCallback(GLFWwindow* window __attribute__((unused)),
+                           double xoffset,
+                           double yoffset) {
+  fprintf(stderr, "Scroll! %.1f, %.1f\n", xoffset, yoffset);
+}
+
+static void ErrorCallback(int error, const char* description)
+{
+  fprintf(stderr, "Error (%d): %s\n", error, description);
+}
+
+static GLFWwindow* OpenglSetup() {
+  glfwSetErrorCallback(ErrorCallback);
+
+  // Load GLFW and Create a Window
+  if (!glfwInit()) {
+    fprintf(stderr, "Failed to initialize glfw");
+    exit(EXIT_FAILURE);
+  }
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+  glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+  glfwWindowHint(GLFW_RESIZABLE, GL_FALSE);
+  GLFWwindow* const window = glfwCreateWindow(800, 600, "basketball", nullptr, nullptr);
+  // Check for Valid Context
+  if (window == nullptr) {
+    fprintf(stderr, "Failed to Create OpenGL Context");
+    glfwTerminate();
+    exit(EXIT_FAILURE);
+  }
+
+  
+  glfwSetKeyCallback(window, KeyCallback);
+  glfwSetCursorPosCallback(window, CursorPositionCallback);
+  glfwSetMouseButtonCallback(window, MouseButtonCallback);
+  glfwSetScrollCallback(window, ScrollCallback);
+ 
+  // Create Context and Load OpenGL Functions
+  glfwMakeContextCurrent(window);
+
+  glewExperimental = GL_TRUE;
+  glewInit();
+  
+  //gladLoadGL();
+  fprintf(stderr, "OpenGL %s\n", glGetString(GL_VERSION));
+  glfwSwapInterval(1);
+
+  return window;
+}
+
+int main(int argc, char * argv[]) {
+  // Parse args
+  if (argc != 2) {
+    fprintf(stderr, "need one argument: court.jpg location\n");
+    exit(EXIT_FAILURE);
+  }
+  const std::string image_path = argv[1];
+
+  // Boilerplate
+  GLFWwindow * const window = OpenglSetup();
+
+  // Create Vertex Array Object
+  GLuint vao;
+  glGenVertexArrays(1, &vao);
+  glBindVertexArray(vao);
+
+  // Create a Vertex Buffer Object and copy the vertex data to it
+  GLuint vbo;
+  glGenBuffers(1, &vbo);
+
+  GLfloat vertices[] = {
+  //  Position      Color             Texcoords
+    -0.5f,  0.5f, 1.0f, 0.0f, 0.0f, 0.0f, 0.0f, // Top-left
+     0.5f,  0.5f, 0.0f, 1.0f, 0.0f, 1.0f, 0.0f, // Top-right
+     0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 1.0f, 1.0f, // Bottom-right
+    -0.5f, -0.5f, 1.0f, 1.0f, 1.0f, 0.0f, 1.0f  // Bottom-left
+  };
+
+  glBindBuffer(GL_ARRAY_BUFFER, vbo);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+  // Create an element array
+  GLuint ebo;
+  glGenBuffers(1, &ebo);
+
+  GLuint elements[] = {
+    0, 1, 2,
+    2, 3, 0
+  };
+
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(elements), elements, GL_STATIC_DRAW);
+
+  // Create and compile the vertex shader
+  GLuint vertexShader = glCreateShader(GL_VERTEX_SHADER);
+  glShaderSource(vertexShader, 1, &vertexSource, NULL);
+  glCompileShader(vertexShader);
+
+  // Create and compile the fragment shader
+  GLuint fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+  glShaderSource(fragmentShader, 1, &fragmentSource, NULL);
+  glCompileShader(fragmentShader);
+
+  // Link the vertex and fragment shader into a shader program
+  GLuint shaderProgram = glCreateProgram();
+  glAttachShader(shaderProgram, vertexShader);
+  glAttachShader(shaderProgram, fragmentShader);
+  glBindFragDataLocation(shaderProgram, 0, "outColor");
+  glLinkProgram(shaderProgram);
+  glUseProgram(shaderProgram);
+
+  // Specify the layout of the vertex data
+  GLint posAttrib = glGetAttribLocation(shaderProgram, "position");
+  glEnableVertexAttribArray(posAttrib);
+  glVertexAttribPointer(posAttrib, 2, GL_FLOAT, GL_FALSE, 7 * sizeof(GLfloat), 0);
+
+  GLint colAttrib = glGetAttribLocation(shaderProgram, "color");
+  glEnableVertexAttribArray(colAttrib);
+  glVertexAttribPointer(colAttrib, 3, GL_FLOAT, GL_FALSE, 7 * sizeof(GLfloat), (void*)(2 * sizeof(GLfloat)));
+
+  GLint texAttrib = glGetAttribLocation(shaderProgram, "texcoord");
+  glEnableVertexAttribArray(texAttrib);
+  glVertexAttribPointer(texAttrib, 2, GL_FLOAT, GL_FALSE, 7 * sizeof(GLfloat), (void*)(5 * sizeof(GLfloat)));
+
+  // Load textures
+  GLuint textures[2];
+  glGenTextures(2, textures);
+
+  int width, height;
+  glActiveTexture(GL_TEXTURE0);
+  glBindTexture(GL_TEXTURE_2D, textures[0]);
+  unsigned char* image = SOIL_load_image("sample.png", &width, &height, 0, SOIL_LOAD_RGB);
+  //unsigned char* image = SOIL_load_image(image_path.c_str(), &width, &height, 0, SOIL_LOAD_RGB);
+  if (image == nullptr) {
+    fprintf(stderr, "Can't load image %s: %s\n", image_path.c_str(), SOIL_last_result());
+    exit(EXIT_FAILURE);
+  }
+  glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, image);
+  SOIL_free_image_data(image);
+  glUniform1i(glGetUniformLocation(shaderProgram, "texKitten"), 0);
+
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+  glActiveTexture(GL_TEXTURE1);
+  glBindTexture(GL_TEXTURE_2D, textures[1]);
+      image = SOIL_load_image("sample2.png", &width, &height, 0, SOIL_LOAD_RGB);
+      glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, image);
+      SOIL_free_image_data(image);
+  glUniform1i(glGetUniformLocation(shaderProgram, "texPuppy"), 1);
+
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+  GLint uniModel = glGetUniformLocation(shaderProgram, "model");
+
+  // Set up projection
+  glm::mat4 view = glm::lookAt(
+      glm::vec3(1.2f, 1.2f, 1.2f),
+      glm::vec3(0.0f, 0.0f, 0.0f),
+      glm::vec3(0.0f, 0.0f, 1.0f)
+  );
+  GLint uniView = glGetUniformLocation(shaderProgram, "view");
+  glUniformMatrix4fv(uniView, 1, GL_FALSE, glm::value_ptr(view));
+
+  glm::mat4 proj = glm::perspective(glm::radians(45.0f), 800.0f / 600.0f, 1.0f, 10.0f);
+  GLint uniProj = glGetUniformLocation(shaderProgram, "proj");
+  glUniformMatrix4fv(uniProj, 1, GL_FALSE, glm::value_ptr(proj));
+
+  auto t_start = std::chrono::high_resolution_clock::now();
+  while (glfwWindowShouldClose(window) == false) {
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+      glfwSetWindowShouldClose(window, true);
+    }
+  
+    // Clear the screen to black
+    glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    // Calculate transformation
+    auto t_now = std::chrono::high_resolution_clock::now();
+    float time = std::chrono::duration_cast<std::chrono::duration<float>>(t_now - t_start).count();
+
+    glm::mat4 model = glm::mat4(1.0f);
+    model = glm::rotate(
+      model,
+      time * glm::radians(180.0f),
+      glm::vec3(0.0f, 0.0f, 1.0f)
+    );
+    glUniformMatrix4fv(uniModel, 1, GL_FALSE, glm::value_ptr(model));
+
+    // Draw a rectangle from the 2 triangles using 6 indices
+    glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+
+    // Swap buffers
+    glfwSwapBuffers(window);
+
+    glfwPollEvents();
+  }
+
+  glDeleteTextures(2, textures);
+
+  glDeleteProgram(shaderProgram);
+  glDeleteShader(fragmentShader);
+  glDeleteShader(vertexShader);
+
+  glDeleteBuffers(1, &ebo);
+  glDeleteBuffers(1, &vbo);
+
+  glDeleteVertexArrays(1, &vao);
+
+  glfwDestroyWindow(window);
+  glfwTerminate();
+  return EXIT_SUCCESS;
+}
